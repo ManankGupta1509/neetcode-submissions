9d8ebff1519d9ad73acca12c81b5e3a69cc8ef86
ManankGupta1509 <44035855+ManankGupta1509@users.noreply.github.com>
2026-08-20 21:29:44 +0530

    Add: longest-common-prefix - submission-6

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-6.cs
@@ -0,0 +1,39 @@
+public class Solution {
+    public string LongestCommonPrefix(string[] strs) {
+        string result = string.Empty;
+        int largestLength = 0;
+        string largestString = string.Empty;
+        bool present = true;
+        foreach(string str in strs)
+        {
+            if(str.Length > largestLength)
+            {
+                largestString = str;
+                largestLength = str.Length;
+            }
+        }
+
+        for (int i=0; i<largestLength; i++)
+        {
+            foreach(string str in strs)
+            {
+                if((i >= str.Length) || (str[i] != largestString[i]))
+                {
+                    present = false;
+                    break;
+                }
+            }
+
+            if(present)
+            {
+                result += largestString[i];
+            }
+            else
+            {
+                break;
+            }
+        }
+        largestString = string.Empty;
+        return result;
+    }
+}
