16473b15f5cba4613a188115a6f31a2d0dd9b149
ManankGupta1509 <44035855+ManankGupta1509@users.noreply.github.com>
2026-08-23 17:11:56 +0530

    Add: valid-sudoku - submission-0

---
+++ Data Structures & Algorithms/valid-sudoku/submission-0.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public bool IsValidSudoku(char[][] board) {
+        var rows = new HashSet<char>[9];
+        var cols = new HashSet<char>[9];
+        var boxes = new HashSet<char>[9];
+        for(int i=0; i<9; i++)
+        {
+            rows[i] = new HashSet<char>();
+            cols[i] = new HashSet<char>();
+            boxes[i] = new HashSet<char>();
+        }
+
+        for(int r=0; r<9; r++)
+        {
+            for(int c=0; c<9; c++)
+            {
+                var val = board[r][c];
+                if(val == '.')
+                    continue;
+                
+                var boxIndex = (r/3)*3+(c/3);
+
+                if(!rows[r].Add(val) || !cols[c].Add(val) || !boxes[boxIndex].Add(val))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
